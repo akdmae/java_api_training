6aeb7042f0028fc630316bb0d7326080baa658e1
Maessarath Akadiri <maessarath.akadiri@epita.fr>
2021-07-11 16:50:41 +0200

    Update Class Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -4,20 +4,17 @@ import java.io.IOException;
 
 public class Launcher {
     //LAUNCH PROGRAM
-    public static void main(String[] args) throws InterruptedException {
-        try {
-            if (args.length == 0)
-            {
-                System.err.println("No Port");
-                System.exit(-1);
-            }
-            else if (args.length >  1)
-            {
-                new SecondServer().Server(Integer.parseInt(args[0]),args[1]);
-            }
-            new StartServer(Integer.parseInt(args[0]));
-        } catch (IOException m_e) {
-            m_e.printStackTrace();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        if (args.length == 0)
+        {
+            System.err.println("No Port");
+            System.exit(-1);
         }
+        else if (args.length >  1)
+        {
+            new SecondServer().Server(Integer.parseInt(args[0]),args[1]);
+        }
+        new StartServer(Integer.parseInt(args[0]));
+
     }
 }
